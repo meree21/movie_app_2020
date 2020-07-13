a21e1239bb5085f671cc5b518c61ddc70910e266
meree21 <67742880+meree21@users.noreply.github.com>
2020-07-13 23:14:20 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 // 카운터
-import React, { Fragment } from 'react';
-import ProTypes from "prop-types";
+import React,{useState} from 'react';
+import { Button } from 'react-bootstrap';
 
-class App extends React.Component{
-  //object
-  state = {
-    count: 0
-  };
-//function(javascirt)
-  add = () => {
-    this.setState({count : + 1})          
-  };
-  minus = () => {
-    this.setState({count : - 1})
-  }; 
- render(){
-   return(
-     <div>
-      <h1> The number id {this.state.count}</h1>
-     // add버튼을 클릭하면 this.add를 실행
-      <button onClick={this.add()}>add</button>
-    // minus버튼을 클릭하면 this.minus를 실행
-      <button onClick={this.minus()}>minus</button>
-     </div>
-   );
- } 
+function App(){
+  //count라는 값을 상태값 정의
+  const [count, setCount] = useState(0);
+
+return(
+  <div>
+  {/*버튼을 누르면 올라가는 숫자*/}
+    <p>Number : {count} </p>
+    {/*버튼을 클릭해 count + 1 */}
+    <Button onClick={() => setCount(count + 1)}>
+      Click me + 
+    </Button>
+    {/*버튼을 클릭해 count - 1 */}
+    <Button onClick={() => setCount(count - 1)}>
+    Click me - 
+  </Button> 
+  </div> 
+);
 }
 
 export default App;
